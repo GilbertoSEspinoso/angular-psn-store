0ae10f6569b99f45f53c1f9aa949ca36fcdde189
Gilberto Espinoso <gilbertosilvaespinosa@gmail.com>
2023-10-13 22:37:21 -0300

    created card-pricing

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
   templateUrl: './card-pricing.component.html',
-  styleUrls: ['./card-pricing.component.css']
+  styleUrls: ['./card-pricing.component.css'],
 })
 export class CardPricingComponent implements OnInit {
+  @Input()
+  gameOrder: string = '';
+  @Input()
+  gameName: string = '';
+  @Input()
+  gamePrice: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
